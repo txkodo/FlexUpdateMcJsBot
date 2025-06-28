a5b892f2eb721c3e85463dbb35bebab62d951f40
txkodo <kodo.hsmkai@gmail.com>
2025-06-29 03:44:18 +0900

    エラーハンドラを追加

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
 import { createBot } from 'mineflayer';
 import { Command } from 'commander';
 
-new Command()
+await new Command()
     .description('棒立ちするだけのMinecraft Botを起動する. Kickされるとプロセスが終了する')
     .showHelpAfterError()
     .argument('<host>', 'ホスト名またはIPアドレス')
     .argument('<port>', 'ポート番号')
     .argument('<username>', 'ユーザー名')
     .argument('<version>', 'Minecraftのバージョン')
-    .action((host, port, username, version) => new Promise<void>((resolve) => {
+    .action((host, port, username, version) => new Promise<void>((resolve,reject) => {
         const bot = createBot({ host, port, username, version, });
         bot.once('spawn', () => {
             console.log('Spawned!');
         });
+        bot.once("error", (err) => {
+            console.error('Error:', err);
+            reject(err);
+        })
         bot.once("kicked", () => resolve())
-    }))
+    })).parseAsync()
